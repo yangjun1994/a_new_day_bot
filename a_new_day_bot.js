fd186b2875b36db02fb471de7d6e52da6d16154f
YangJun <yangjun@1994.io>
2020-05-19 22:13:28 +0800

    add code

---
+++ a_new_day_bot.js
@@ -0,0 +1,113 @@
+const TelegramBot = require('node-telegram-bot-api');
+const schedule = require('node-schedule');
+const express = require('express');
+const bodyParser = require('body-parser');
+const request = require('request');
+const sd = require('silly-datetime');
+const fs = require('fs');
+
+
+const TOKEN = 'your token';
+const url = 'https://your.webhook.url';
+const port = 9001;
+
+
+let enablelist = new Array();
+function savelist () {
+  fs.writeFile("enablelist.txt",JSON.stringify(enablelist), err => {
+      if(!err) console.log("save success");
+  });
+}
+function loadlist () {
+  enablelist = JSON.parse(fs.readFileSync('enablelist.txt'));
+  console.log(enablelist);
+  console.log("load success");
+}
+
+loadlist();
+const bot = new TelegramBot(TOKEN);
+bot.setWebHook(`${url}/bot${TOKEN}`);
+const app = express();
+
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => res.send(`I am a Telegram Bot`));
+
+app.post(`/bot${TOKEN}`, (req, res) => {
+  bot.processUpdate(req.body);
+  res.sendStatus(200);
+});
+
+app.listen(port, () => {
+  console.log(`Express server is listening on ${port}`);
+});
+
+bot.onText(/\/getid_a_new_day_bot/, function onLoveText(msg) {
+    const chatId = msg.chat.id;
+    console.log(`getid_a_new_day_bot`+msg.chat.id)
+    bot.sendMessage(chatId, chatId);
+});
+
+bot.onText(/\/enable_a_new_day_bot/, function onLoveText(msg) {
+  const chatId = msg.chat.id;
+  console.log(`enable_a_new_day_bot`+msg.chat.id)
+  if (enablelist.includes(chatId)) {
+    bot.sendMessage(chatId, '之前就enable了2333');
+  }
+  else {
+    enablelist.push(chatId);
+    savelist();
+    bot.sendMessage(chatId, '200 OK（假装自己是http server）');
+
+  }
+});
+
+bot.onText(/\/disable_a_new_day_bot/, function onLoveText(msg) {
+  const chatId = msg.chat.id;
+  console.log(`disable_a_new_day_bot`+msg.chat.id)
+  if (enablelist.includes(chatId)) {
+    enablelist.pop(chatId);
+    savelist();
+    bot.sendMessage(chatId, '200 OK（假装自己是http server）');
+  }
+  else {
+    bot.sendMessage(chatId, '并未enable了2333');
+  }
+});
+
+bot.onText(/\/status_a_new_day_bot/, function onLoveText(msg) {
+  const chatId = msg.chat.id;
+  console.log(`status_a_new_day_bot`+msg.chat.id)
+  if (enablelist.includes(chatId)) {
+    bot.sendMessage(chatId, '已启用');
+  }
+  else {
+    bot.sendMessage(chatId, '未启用');
+
+  }
+});
+
+const sendmessageanewday = ()=>{
+    schedule.scheduleJob('0 0 0 * * *',()=>{
+      enablelist.forEach(function(x){
+        console.log('send to:'+x)
+        bot.sendMessage(x, '新的一天开始了！');
+      });
+    });
+}
+
+
+sendmessageanewday();
+
+const sendmessageanewdayhour = ()=>{
+  schedule.scheduleJob('0 0 * * * *',()=>{
+    enablelist.forEach(function(x){
+      console.log('send to:'+x)
+      let timenow=sd.format(new Date(), 'YYYY-MM-DD HH:mm:00');
+      bot.sendMessage(x, '新的一小时开始了！' + '现在是北京时间 '+timenow);
+    });
+  });
+}
+
+
+sendmessageanewdayhour();
